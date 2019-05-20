db8b3bd83ec9745cd5121b10567ed6679ab02c4a
SKrivanec <skrivanec@inwk.com>
2019-05-20 08:51:30 +0200

    CH: AngularJS Tables, complete

--- app/012-Tables/configAndController.js
+++ app/012-Tables/configAndController.js
@@ -11,7 +11,17 @@ angular.module('myApp.012tables', ['ngRoute'])
 
     .controller('tablesCtrl', ['$scope',
         function ($scope) {
+            $scope.names = [
+                {name: 'Jani', country: 'Norway'},
+                {name: 'Carl', country: 'Sweden'},
+                {name: 'Margareth', country: 'England'},
+                {name: 'Hege', country: 'Norway'},
+                {name: 'Joe', country: 'Denmark'},
+                {name: 'Gustav', country: 'Sweden'},
+                {name: 'Birgit', country: 'Denmark'},
+                {name: 'Mary', country: 'England'},
+                {name: 'Kai', country: 'Norway'}
+            ];
 
-
-    }])
+        }])
 ;
