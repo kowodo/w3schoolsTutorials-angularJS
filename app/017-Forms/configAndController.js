da25d20b317b0c80436ae1145c92f3726d591ba6
SKrivanec <skrivanec@inwk.com>
2019-05-20 10:18:12 +0200

    CH: AngularJS Forms > complete

--- app/017-Forms/configAndController.js
+++ app/017-Forms/configAndController.js
@@ -11,6 +11,10 @@ angular.module('myApp.017forms', ['ngRoute'])
 
     .controller('formsCtrl', ['$scope',
         function ($scope) {
-
+            $scope.master = {firstName: "John", lastName: "Doe"};
+            $scope.reset = function() {
+                $scope.user = angular.copy($scope.master);
+            };
+            $scope.reset();
         }])
 ;
