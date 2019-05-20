acc7330040f576181cf2c6f86ca177c53e072774
SKrivanec <skrivanec@inwk.com>
2019-05-20 09:27:16 +0200

    CH: AngularJS Select Boxes > complete

--- app/013-SelectBoxes/configAndController.js
+++ app/013-SelectBoxes/configAndController.js
@@ -11,6 +11,21 @@ angular.module('myApp.013selectboxes', ['ngRoute'])
 
     .controller('selectBoxesCtrl', ['$scope',
         function ($scope) {
-
+            $scope.names = ["Emil", "Tobias", "Linus"];
+            $scope.cars = [
+                {model : "Ford Mustang", color : "red"},
+                {model : "Fiat 500", color : "white"},
+                {model : "Volvo XC90", color : "black"}
+            ];
+            $scope.carsObject = {
+                car01 : "Ford",
+                car02 : "Fiat",
+                car03 : "Volvo"
+            };
+            $scope.carsObject2 = {
+                car01 : {brand : "Ford", model : "Mustang", color : "red"},
+                car02 : {brand : "Fiat", model : "500", color : "white"},
+                car03 : {brand : "Volvo", model : "XC90", color : "black"}
+            };
         }])
 ;
