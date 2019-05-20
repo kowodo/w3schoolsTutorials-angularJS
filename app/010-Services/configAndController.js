75052159244bcae8f4fda5917bd0d80957a01ea2
SKrivanec <skrivanec@inwk.com>
2019-05-20 08:32:17 +0200

    CH: AngularJS Services, complete

--- app/010-Services/configAndController.js
+++ app/010-Services/configAndController.js
@@ -9,13 +9,30 @@ angular.module('myApp.010Services', ['ngRoute'])
         });
     }])
 
-    .controller('ServicesCtrl', ['$scope', '$location', '$http', function ($scope, $location, $http) {
-        $scope.myUrl = $location.absUrl();
-        $http.get("welcome.html").then(function(response){
-            $scope.myWelcome = response.data;
-        })
+    .controller('ServicesCtrl', ['$scope', '$location', '$http', '$timeout', '$interval', 'hexafy',
+        function ($scope, $location, $http, $timenout, $interval, hexafy) {
+        // $http.get("welcome.html").then(function(response){
+        //     $scope.myWelcome = response.data;
+        //     $scope.myHeader = "Hello World!";
+        // })
+        $scope.myHeader = "Hello World!<<";
+        $timenout(function () {
+            $scope.myHeader = "How are you today?";
+        }, 2000);
+
+        $scope.theTime = new Date().toLocaleDateString();
+        $interval(function () {
+            $scope.theTime = new Date();
+        }, 1000);
+
+        $scope.hex = hexafy.myFunc(255);
 
     }])
+    .service('hexafy', function () {
+        this.myFunc = function (x) {
+            return x.toString(16);
+        }
+    })
 
 
 ;
